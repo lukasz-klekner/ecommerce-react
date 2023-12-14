1abe67c345dbf4eead4a129f90f7ff2f260d089b
Luki Klekner <lukaszklekner@gmail.com>
2023-12-14 16:43:14 +0100

    feat: add theme and global styles

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import "./styles/theme.css"
+import "./styles/global.css"
+
+import App from './App.jsx'
+
 
 const router = createBrowserRouter([
   {
