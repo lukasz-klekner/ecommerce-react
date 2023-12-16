0f4b3355a7a331a810bba8797bf2fc254906725d
Luki Klekner <lukaszklekner@gmail.com>
2023-12-16 14:28:32 +0100

    feat: create Logo component

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Footer } from "../Footer/Footer";
+import { Logo } from "../Logo/Logo";
 import { MainMenu } from "../MainMenu/MainMenu";
 
 export const Layout = () => (
     <>
         <MainMenu />
+        <Logo />
         <Footer/>
     </>
 )
---
+++ front-end/src/components/Logo/Logo.jsx
@@ -0,0 +1,5 @@
+import styles from "./Logo.module.css"
+
+export const Logo = () => (
+    <h1 className={styles.logo}>TopSklep®</h1>
+)
