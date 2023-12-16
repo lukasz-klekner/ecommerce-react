69b50b1d19932482a6c282481a11293a8390157e
Luki Klekner <lukaszklekner@gmail.com>
2023-12-16 18:15:27 +0100

    feat: create MainContent component

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -3,20 +3,23 @@ import { CurrencySelector } from "../CurrencySelector/CurrencySelector";
 import { Footer } from "../Footer/Footer";
 import { IconMenu } from "../IconMenu/IconMenu";
 import { Logo } from "../Logo/Logo";
+import { MainContent } from "../MainContent/MainContent";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { TopBar } from "../TopBar/TopBar";
 
 export const Layout = () => (
     <>
-        <TopBar>
-            <MainMenu />
-            <Logo />
-            <div>
-                <CurrencySelector />
-                <IconMenu />
-            </div>
-        </TopBar>
-        <CategoryMenu />
+        <MainContent>
+            <TopBar>
+                <MainMenu />
+                <Logo />
+                <div>
+                    <CurrencySelector />
+                    <IconMenu />
+                </div>
+            </TopBar>
+            <CategoryMenu />
+        </MainContent>
         <Footer/>
     </>
 )
