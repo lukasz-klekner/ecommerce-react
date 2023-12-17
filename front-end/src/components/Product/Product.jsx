bc38ffc584be78a070245fbd0f5c66966a29aef6
Luki Klekner <lukaszklekner@gmail.com>
2023-12-17 09:01:32 +0100

    fix: improve Product component and style it

--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import styles from "./Product.module.css"
+
 export const Product = ({ product }) => (
-    <p>{product.productName}</p>
+    <Link className={styles.product}>
+        <img src={product.photos[0]}/>
+        <h3>{product.productName}</h3>
+        <p>{product.pricePLN}zł</p>
+        <div className={styles.heart}></div>
+    </Link>
 )
