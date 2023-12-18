983a5e41ca767e9a2928642871d153dee3be71e3
Luki Klekner <lukaszklekner@gmail.com>
2023-12-18 13:50:39 +0100

    fix: add cost variables to CartSummary

--- front-end/src/components/CartSummary/CartSummary.jsx
+++ front-end/src/components/CartSummary/CartSummary.jsx
@@ -3,25 +3,38 @@ import styles from "./CartSummary.module.css"
 import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
 import CAR_ICON from "../../assets/car.svg"
 
-export const CartSummary = ({ products }) => (
-    <div className={styles.cartSummary}>
-        <h2>Podsumowanie</h2>
-        <div className={styles.cartRow}>
-            <p>Wartość produktów:</p>
-            <p>199zł</p>
-        </div>
-        <div className={styles.cartRow}>
-            <p>Koszt dostawy:</p>
-            <p>9,99zł</p>
-        </div>
-        <div className={`${styles.cartRow} ${styles.cartSummaryRow}`}>
-            <p>Do zapłaty:</p>
-            <p>208,99zł</p>
-        </div>
-        <FullWidthButton isBlack>Do zapłaty</FullWidthButton>
-        <div className={styles.deliveryInfo}>
-            <img src={CAR_ICON} />
-            <p>Darmowa dostawa od 500zł</p>
-        </div>
-    </div>
-)
+export const CartSummary = ({ products }) => {
+    const deliveryCost = 49;
+    const minSumForFreeDelivery = 500;
+
+    let sum = 0
+
+    products.forEach(product => {
+        sum += product.pricePLN
+    })
+
+    const totalCost = sum > minSumForFreeDelivery ? sum : sum + deliveryCost
+
+    return (
+        <div className={styles.cartSummary}>
+            <h2>Podsumowanie</h2>
+            <div className={styles.cartRow}>
+                <p>Wartość produktów:</p>
+                <p>{sum}zł</p>
+            </div>
+            <div className={styles.cartRow}>
+                <p>Koszt dostawy:</p>
+                <p>{sum > minSumForFreeDelivery ? 0 : deliveryCost}zł</p>
+            </div>
+            <div className={`${styles.cartRow} ${styles.cartSummaryRow}`}>
+                <p>Do zapłaty:</p>
+                <p>{totalCost}zł</p>
+            </div>
+            <FullWidthButton isBlack>Do zapłaty</FullWidthButton>
+            <div className={styles.deliveryInfo}>
+                <img src={CAR_ICON} />
+                <p>Darmowa dostawa od {minSumForFreeDelivery}zł</p>
+            </div>
+       </div>
+    )
+} 
