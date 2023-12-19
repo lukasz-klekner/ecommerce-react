d852c911819bce09ef5d386c9af5400abd153749
Luki Klekner <lukaszklekner@gmail.com>
2023-12-19 08:27:06 +0100

    fix: add headerText to BestSellers component

--- front-end/src/components/BestSellers/BestSellers.jsx
+++ front-end/src/components/BestSellers/BestSellers.jsx
@@ -2,9 +2,9 @@ import { CenteredContent } from "../CenteredContent/CenteredContend"
 import { Product } from "../Product/Product"
 import styles from "./BestSellers.module.css"
 
-export const BestSellers = ({ products }) => (
+export const BestSellers = ({ products,headerText }) => (
     <CenteredContent>
-        <h2 className={styles.bestSellersHeader}>Sprawdź nasze bestellery</h2>
+        <h2 className={styles.bestSellersHeader}>{headerText}</h2>
         <div className={styles.productWrapper}>
             {products.map(product => (
                 <Product key={product.id} product={product} />
