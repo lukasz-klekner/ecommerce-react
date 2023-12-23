346fffe0404212386e2e568d6c6345285d280472
Luki Klekner <lukaszklekner@gmail.com>
2023-12-23 12:49:24 +0100

    feat: unmock breadcrumbs and integrate with router

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,28 +1,38 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import styles from "./Breadcrumbs.module.css"
 import ARROW_ICON from "../../assets/arrow.svg"
+import { CATEGORIES, GENDERS } from "../../constants/categories"
 
 export const Breadcrumbs = () => {
-    const BREADCRUMBS = [
+    const { gender, category, subcategory } = useParams()
+
+    const foundGender = GENDERS.find(({ path }) => gender === path)
+    const foundCategory = CATEGORIES.find(({ path }) => category === path)
+
+    const breadcrumbs = [
         {
-            categoryName: "Kobieta",
-            path: "kobieta",
+            categoryName: foundGender.categoryName,
+            path: `/${foundGender.path}`,
         },
         {
-            categoryName: "Odzież",
-            path: "odziez",
-        },
-        {
-            categoryName: "Swetry",
-            path: "swetry",
+            categoryName: foundCategory.categoryName,
+            path: `/${foundGender.path}/${foundCategory.path}`,
         },
     ]
 
+    if(subcategory){
+        const foundSubcategory = foundCategory.subcategories.find(({ path }) => path === subcategory)
+        breadcrumbs.push({
+            categoryName: foundSubcategory.categoryName,
+            path: `/${foundGender.path}/${foundCategory.path}/${foundSubcategory.categoryName}`,            
+        })
+    }
+
     return (
         <ul className={styles.breadcrumbs}>
-            {BREADCRUMBS.map(({ path, categoryName }) =>(
+            {breadcrumbs.map(({ path, categoryName }) =>(
                 <li key={path}>
-                    <NavLink to={path}>
+                    <NavLink end to={path}>
                         {categoryName}
                         <img src={ARROW_ICON} />
                     </NavLink>
